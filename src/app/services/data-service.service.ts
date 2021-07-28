dd425ad25be247298be21473ff2cf3671054a185
zZyzo <khamidulin.ilgizar@gmail.com>
2021-07-28 14:18:20 +0300

    0.3.6
    Sorting

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import { BehaviorSubject, from, ReplaySubject, Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 
@@ -15,14 +15,40 @@ export interface TaskElement {
 })
 export class DataServiceService {
 
-  public tasks$ = new BehaviorSubject<TaskElement[]>([]);
-
+  public tasks$ = new BehaviorSubject<TaskElement[]>([
+    //Fake data//
+    {
+      name: "Task 1",
+      description: "Lorem ipsum",
+      date: "28 Jul 2021 13:41",
+      status: "Done",
+    },
+    {
+      name: "Task 2",
+      description: "Lorem ipsum",
+      date: "29 Jul 2021 13:42",
+      status: "ToDo",
+    },
+    {
+      name: "Task 3",
+      description: "Lorem ipsum",
+      date: "9 Jul 2021 13:45",
+      status: "InProgress",
+    },
+    {
+      name: "Task 4",
+      description: "Lorem ipsum",
+      date: "31 Jul 2021 13:46",
+      status: "Done",
+    }
+  ]);
+  //----//
   constructor() {}
-  
+
   getTasksStream(){
     return this.tasks$;
   }
-  
+
   addTask(task: TaskElement){
     this.tasks$.next([...this.tasks$.getValue(), task])
   }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { iif, Observable, pipe, Subject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { DataServiceService, TaskElement } from '../services/data-service.service';
 import { ToastService } from '../services/toast.service';
 
@@ -11,16 +13,22 @@ import { ToastService } from '../services/toast.service';
 })
 
 export class TableComponent implements OnInit {
-  name = new FormControl('');
+  status = new FormControl("All");
+  tasks$: Observable<TaskElement[]>;
+
   columns = [{ prop: 'name' }, { name: 'Description' }, { name: 'Date' }, { name: 'Status' }];
   
   constructor(public dataService: DataServiceService, public router:Router, public toastService: ToastService) {
+    this.tasks$ = this.dataService.getTasksStream();
   }
-  
-  ngOnInit(): void {}
-  
-  // deleteTask(){
-  //   this.tasks = this.tasks.filter(t => t.name !== this.name);
-  //   this.name = '';
-  // }
+
+  ngOnInit(): void {
+    this.status.valueChanges.subscribe(val => {
+      this.tasks$ = this.dataService.getTasksStream();
+      if(val !== 'All'){
+        this.tasks$ = this.tasks$.pipe(
+          map(tasks => tasks.filter(task => task.status === val)))
+      }
+    });
+  }
 }
