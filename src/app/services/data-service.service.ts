a8a8c490f11d0968447881cd307bb1a8d7b97449
zZyzo <khamidulin.ilgizar@gmail.com>
2021-07-29 18:00:21 +0300

    Update data-service.service.ts

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -15,34 +15,8 @@ export interface TaskElement {
 })
 export class DataServiceService {
 
-  public tasks$ = new BehaviorSubject<TaskElement[]>([
-    //Fake data//
-    {
-      name: "Task 1",
-      description: "Lorem ipsum",
-      date: "28 Jul 2021 13:41",
-      status: "Done",
-    },
-    {
-      name: "Task 2",
-      description: "Lorem ipsum",
-      date: "29 Jul 2021 13:42",
-      status: "ToDo",
-    },
-    {
-      name: "Task 3",
-      description: "Lorem ipsum",
-      date: "9 Jul 2021 13:45",
-      status: "InProgress",
-    },
-    {
-      name: "Task 4",
-      description: "Lorem ipsum",
-      date: "31 Jul 2021 13:46",
-      status: "Done",
-    }
-  ]);
-  //----//
+  public tasks$ = new BehaviorSubject<TaskElement[]>([]);
+
   constructor() {}
 
   getTasksStream(){
