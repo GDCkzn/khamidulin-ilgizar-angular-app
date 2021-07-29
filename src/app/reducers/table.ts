a4388035317f233aa368caa1ff4e61e125c1422d
zZyzo <khamidulin.ilgizar@gmail.com>
2021-07-29 17:59:30 +0300

    Update table.ts

--- src/app/reducers/table.ts
+++ src/app/reducers/table.ts
@@ -12,7 +12,32 @@ export interface TableState{
 }
 
 export const initialState: TableState = {
-    data: []
+    data: [
+        {
+            name: "Task 1",
+            description: "Lorem ipsum",
+            date: "28 Jul 2021 13:41",
+            status: "Done",
+        },
+        {
+          name: "Task 2",
+          description: "Lorem ipsum",
+          date: "29 Jul 2021 13:42",
+          status: "ToDo",
+        },
+        {
+          name: "Task 3",
+          description: "Lorem ipsum",
+          date: "9 Jul 2021 13:45",
+          status: "InProgress",
+        },
+        {
+          name: "Task 4",
+          description: "Lorem ipsum",
+          date: "31 Jul 2021 13:46",
+          status: "Done",
+        }
+    ]
 };
 
 export const tableReducer = createReducer(
